87bc5439200587669550006b1ab4271659b338cc
Benjamin Fry <benjaminfry@me.com>
2018-10-28 01:05:57 -0700

    stubs for listen and bind

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+use std::mem;
+
+use quinn::{Endpoint, EndpointBuilder};
+
+#[no_mangle]
+pub extern "C" fn new_endpoint_builder<'a>() -> *mut EndpointBuilder<'a> {
+    Box::into_raw(Box::new(Endpoint::new()))
+}
+
+#[no_mangle]
+pub extern "C" fn listen<'a>(builder: *mut EndpointBuilder<'a>) {
+    assert!(!builder.is_null());
+    let builder: &mut EndpointBuilder = unsafe { &mut *builder };
+
+    // builder.listen
+}
+
+#[no_mangle]
+pub extern "C" fn bind<'a>(builder: *mut EndpointBuilder<'a>) {
+    assert!(!builder.is_null());
+    let builder: &mut EndpointBuilder = unsafe { &mut *builder };
+
+    // builder.bind
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
